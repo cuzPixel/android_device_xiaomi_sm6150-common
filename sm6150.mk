4ef2f067f3ae3292db5511ccf7180b8a226f8115
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2020-04-23 19:36:35 +0530

    sm6150-common: display: add libdisplayconfig.qti to product packages
    Change-Id: Ic8a69bc5368f9d41c5958181ecb091e6d0d4ad05

--- sm6150.mk
+++ sm6150.mk
@@ -397,6 +397,7 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libdisplayconfig.vendor \
+    libdisplayconfig.qti \
     libnl \
     libqdMetaData \
     libqdMetaData.system \
